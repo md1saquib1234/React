83aebe4f10570acf7bf67ec6f686406496a1c29e
MD SAQUIB ANSARI <mdshaanansari0786@gmail.com>
2026-02-10 16:53:55 +0530

    feat: add Container component for layout structure and update App component to use it fix: update Item component to include list-group-item class and comment out background color

--- 18 Fragments/learning-fragment/src/App.jsx
+++ 18 Fragments/learning-fragment/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from "/vite.svg";
 import FoodItems from "./Components/FoodItems";
 import ErrorMessage from "./Components/ErrorMessage";
 import "./App.css";
+import Container from "./Components/Container";
 
 function App() {
    let foodItems = ["Sabzi", "Green Vegetable", "Roti", "Salad", "Milk", "Ghee"];
@@ -11,11 +12,11 @@ function App() {
  
  
   return (
-    <>
+    <Container>
       <h1 className="food-heading">Healthy Food</h1>
       <ErrorMessage items={foodItems}></ErrorMessage>
       <FoodItems items={foodItems}></FoodItems>
-      </>
+      </Container>
    
   );
 }
--- 18 Fragments/learning-fragment/src/Components/Item.jsx
+++ 18 Fragments/learning-fragment/src/Components/Item.jsx
@@ -3,7 +3,7 @@ import styles from './Item.module.css';
 const Item = ({foodItem}) => {
  console.log(styles);
 return  ( 
-<li  className={`${styles['kg-item']}`}>
+<li  className={`${styles['kg-item']} list-group-item`}>
   <span className={styles['kg-span']}>{foodItem}</span>
   </li>
 );
