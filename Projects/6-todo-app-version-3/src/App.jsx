2307633b2ea7b246f8890557de6aa89c18426650
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-06-12 16:49:27 +0530

    feat: implement todo app functionality with add and delete features, and add welcome message component

--- Projects/6-todo-app-version-3/src/App.jsx
+++ Projects/6-todo-app-version-3/src/App.jsx
@@ -4,33 +4,37 @@ import viteLogo from "/vite.svg";
 import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
 import TodoItem from "./components/TodoItem";
+import WelcomeMessage from "./components/WelcomeMessage";
 import TodoItems from "./components/TodoItems";
 import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   
-  const todoItems = [
-    { name: 'Buy Milk',
-      dueDate: '4/10/2023',
-    },
-      { name: 'Go to College',
-      dueDate: '4/10/2023',
-    },
-    { name: 'Go to Gym',
-      dueDate: '4/10/2023',
-    },
-    {
-      name: 'Go to Market',
-      dueDate: '4/10/2023',
-    }
-  ];
+ 
+
+  const [todoItems, setTodoItems] = useState([]);
+
+  const handleNewItem = (itemName, itemDueDate) => {
+    console.log(`New Item Added: ${itemName} Date:${itemDueDate}` );
+    const newTodoItem = [...todoItems,
+       {name: itemName, dueDate: itemDueDate},
+      ];
+      setTodoItems(newTodoItem);
+  }
+
+  const handleDeleteItem = (todoItemName) => {
+    const newTodoItems = todoItems.filter(item => item.name !== todoItemName);
+    setTodoItems(newTodoItems);
+
+  }
 
   return (
     <center className="todo-container">
        <AppName></AppName>
-        <AddTodo></AddTodo>
-        <TodoItems todoItems={todoItems}></TodoItems>
+        <AddTodo onNewItem={handleNewItem}></AddTodo>
+        {todoItems.length === 0 && <WelcomeMessage ></WelcomeMessage>}
+        <TodoItems todoItems={todoItems} onDeleteClick={handleDeleteItem}></TodoItems>
         
        
       
