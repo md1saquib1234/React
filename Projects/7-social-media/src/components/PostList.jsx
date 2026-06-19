d187f95e238c4ade701eacbf3a94c31e9d1d53be
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-06-19 15:09:44 +0530

    feat: add LoadingSpinner component and integrate it into PostList for loading state indication

--- Projects/7-social-media/src/components/PostList.jsx
+++ Projects/7-social-media/src/components/PostList.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import {PostList as PostListData}  from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
 
 const PostList = () => {
@@ -25,10 +26,13 @@ const PostList = () => {
 
   return (
   <>
+  {/* {fetching &&*/  <LoadingSpinner/>} 
   {
+    !fetching &&
     postList.length === 0 && <WelcomeMessage />
   }
   {
+     !fetching &&
   postList.map((post) => (
     <Post key={post.id} post={post}/>
   ))}
