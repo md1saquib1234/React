396b5640f27537b9616c98ff79089494ac39da5a
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-06-19 14:11:41 +0530

    feat: refactor PostList and WelcomeMessage components to remove button for fetching posts and implement automatic fetching on mount

--- Projects/7-social-media/src/components/WelcomeMessage.jsx
+++ Projects/7-social-media/src/components/WelcomeMessage.jsx
@@ -1,9 +1,7 @@
 const WelcomeMessage = ({onGetPostsClick}) => {
   return <center>
   <h1 className="welcome-message">There are no posts</h1>
-  <button type="button"
-  onClick={onGetPostsClick}
-  className="btn btn-primary">Get Posts from Server</button>
+  
   </center>
 };
 
