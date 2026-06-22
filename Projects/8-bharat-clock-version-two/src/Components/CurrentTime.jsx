f06f8eb8dba5302c56e4190a938a5771c132044e
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-06-22 10:28:13 +0530

    feat: Initialize Todo App with React and Vite
    - Created package.json for project dependencies and scripts.
    - Added Vite logo SVG for branding.
    - Implemented basic styling in App.css.
    - Developed main App component with context provider for todo items.
    - Included React and Vite logos in assets.
    - Created AddTodo component for adding new todo items.
    - Built AppName component with custom styling.
    - Developed TodoItem component for displaying individual todo items.
    - Created TodoItems component to list all todo items.
    - Added WelcomeMessage component to greet users.
    - Implemented context store for managing todo items with useReducer.
    - Configured Vite for project build and development.
    - Updated CurrentTime component to remove console logs.
    - Cleaned up main entry point for better readability.

--- Projects/8-bharat-clock-version-two/src/Components/CurrentTime.jsx
+++ Projects/8-bharat-clock-version-two/src/Components/CurrentTime.jsx
@@ -2,16 +2,15 @@ import { useEffect, useState } from "react";
 
 let CurrentTime = () => {
   const [time, setTime] = useState(new Date());
-  
+
   useEffect(() => {
-    console.log("Interval has been setup");
     const intervalId = setInterval(() => {
       setTime(new Date());
     }, 1000);
 
     return () => {
       clearInterval(intervalId);
-      console.log("Cancel the interval")
+      
     }
   }, []);
 
