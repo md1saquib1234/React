f5e8919aec0eaeab37ce9aaaf9eccc41e0de3c49
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-06-24 10:56:30 +0530

    Add initial setup for React-Redux counter application
    - Created main application structure with App component.
    - Added CSS file for styling.
    - Included SVG icons for various functionalities.
    - Set up Vite configuration for React.
    - Added assets including images and SVGs.

---
+++ 52 Redux/Counter-React-Redux/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <center>
+    <div>
+      <h1>Welcome to react-redux</h1>
+      </div>
+     </center>
+            
+
+      
+    
+  )
+}
+
+export default App
