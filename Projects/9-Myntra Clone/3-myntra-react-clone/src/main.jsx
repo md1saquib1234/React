fdab09f574cb2da0fa0a899816c91bf2ce07aaf0
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-06-26 20:37:56 +0530

    Refactor: Remove unused components and Redux slices
    - Deleted components: BagItem, BagSummary, FetchItems, Footer, Header, HomeItem, LoadingSpinner
    - Removed Redux slices: bagSlice, fetchStatusSlice, itemsSlice
    - Updated main entry point to simplify rendering
    - Added Vite SVG logo asset
    - Cleaned up index.css by removing all styles

--- Projects/9-Myntra Clone/3-myntra-react-clone/src/main.jsx
+++ Projects/9-Myntra Clone/3-myntra-react-clone/src/main.jsx
@@ -1,31 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./routes/App.jsx";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Bag from "./routes/Bag.jsx";
-import Home from "./routes/Home.jsx";
-import { Provider } from "react-redux";
-import myntraStore from "./store/index.js";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { path: "/", element: <Home /> },
-      {
-        path: "/bag",
-        element: <Bag />,
-      },
-    ],
-  },
-]);
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={myntraStore}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
