5c426899f74a00d413d5f5301dce204635823f33
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-06-26 12:12:38 +0530

    feat: refactor store setup to use Redux Toolkit and update counter state management

--- 52 Redux/3-React-Redux - toolkit/src/components/DisplayCounter.jsx
+++ 52 Redux/3-React-Redux - toolkit/src/components/DisplayCounter.jsx
@@ -2,13 +2,14 @@ import { useSelector } from "react-redux";
 
 const DisplayCounter = () => {
 
-  const counter = useSelector(store => store.counter);
+  const {counterVal} = useSelector(store => store.counter);
+  
 
 
   return (
    
      <p className="lead mb-4">
-          Counter Current Value: {counter}
+          Counter Current Value: {counterVal}
         </p>
 
 
