3dc36128e47731f64d5cf152e79805015baae22a
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-06-26 14:25:50 +0530

    feat: Initialize Myntra React Clone project with essential components and Redux setup
    - Added package.json with dependencies and scripts for development
    - Included images for the project in the public/images directory
    - Created core components: Header, Footer, HomeItem, BagItem, BagSummary, FetchItems, and LoadingSpinner
    - Implemented routing with React Router for Home and Bag pages
    - Set up Redux store with slices for managing bag items and fetch status
    - Styled components with CSS for a cohesive look and feel
    - Configured Vite for development and build processes

--- 52 Redux/3-React-Redux - toolkit/src/components/Controls.jsx
+++ 52 Redux/3-React-Redux - toolkit/src/components/Controls.jsx
@@ -1,45 +1,37 @@
 import { useRef } from "react";
 import { useDispatch } from "react-redux";
-import { counterActions } from "../store";
+import { counterActions } from "../store/counter";
+import { privacyActions } from "../store/privacy";
+
 
 const Controls = () => {
 
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const inputElement = useRef();
 
   const handleIncrement = () => {
-    counterActions.increment();
+    dispatch(counterActions.increment());
   }
 
   const handleDecrement = () => {
-    counterActions.decrement();
+    dispatch(counterActions.decrement());
+    
   }
 
   const handlePrivayToggle = () => {
-    dispatch({
-      type: "PRIVACY_TOGGLE" 
-    })
+    dispatch(privacyActions.toggle());
   }
 
   const handleAdd = () => {
-    dispatch({
-      type: "ADD", 
-      payload: {
-       num: inputElement.current.value,
-      },
-    
-  });
+  dispatch(counterActions.add(
+     inputElement.current.value)
+);
      inputElement.current.value = "";
   };
 
   const handleSubtract = () => {
-    dispatch({
-      type: "SUBTRACT", 
-      payload: {
-       num: inputElement.current.value,
-      },
-    
-  });
+    dispatch(counterActions.subtract(
+     inputElement.current.value));
      inputElement.current.value = "";
   };
 
--- 52 Redux/3-React-Redux - toolkit/src/store/index.js
+++ 52 Redux/3-React-Redux - toolkit/src/store/index.js
@@ -1,51 +1,17 @@
 
 import {configureStore, createSlice} from "@reduxjs/toolkit";
-
-// const INITIAL_VALUE = {
-//   counter: 0,
-//   privacy: false
-// }
-
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState: {counterVal: 0},
-  reducers: {
-    increment: (state, action) => {
-      console.log(state, action);
-    },
-    decrement: (state, action) => {
-      console.log(state, action);
-    },
-    add: (state, action) => {
-      console.log(state, action);
-    },
-    subtract: (state, action) => {
-      console.log(state, action);
-    }
-  }
-})
+import counterSlice from "./counter";
+import privacySlice from "./privacy";
 
 
 
 const counterStore = configureStore({reducer: {
-  counter: counterSlice.reducer
+  counter: counterSlice.reducer,
+  privacy: privacySlice.reducer
 }});
 
-export const counterActions = counterSlice.actions;
+
+
 export default counterStore;
 
 
-// const counterReducer = (store =  INITIAL_VALUE, action) => {
-//   if (action.type === 'INCREMENT') {
-//       return {...store, counter: store.counter + 1};
-//   } else if (action.type === 'DECREMENT') {
-//       return {...store,counter: store.counter - 1};
-//   } else if (action.type === 'ADD') {
-//       return {...store, counter: store.counter + Number(action.payload.num)};
-//   } else if (action.type === 'SUBTRACT') {
-//       return {...store, counter: store.counter - Number(action.payload.num)};
-//   } else if (action.type === 'PRIVACY_TOGGLE') {
-//     return {...store, privacy: !store.privacy};
-//   }
-//   return store;
-// }
